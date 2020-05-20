995fc9b329d67026ca47a1f80ecf5ac75c2c54f2
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-05-20 17:36:53 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -4,8 +4,22 @@ namespace hsrgbband
     export function sendBuffer(buf: Buffer, pin: DigitalPin) {
     }
 
+    //% weight=90
+    //% blockId=callSend block="callSend"
+    //% color.fieldEditor="gridpicker"
+    export function callSend(value: number, offset: number, length1: number, pin: DigitalPin): void {
+
+        let strip = new Strip();
+       
+        strip.buf = pins.createBuffer(60 * 3);
+        strip.pin = pin
+        strip.buf.fill(value, offset, length1);
+        strip.callSend();
+    }
+
     export class Strip {
         buf: Buffer;
+        pin: DigitalPin
 
 
         callSend() {
